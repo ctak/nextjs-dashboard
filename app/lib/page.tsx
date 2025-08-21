7d3aa85d2602a6b8c3139d6e932a2fb24f3c0749
c tak <tak.changbeom@gmail.com>
2025-08-21 13:56:57 +0900

    /app 의 모든 폴더는 route 된다. ex) /app/lib/page.tsx

---
+++ app/lib/page.tsx
@@ -0,0 +1,10 @@
+export default async function Page() {
+  return (
+    <main>
+      <h1>
+        /app/lib/page.tsx도 router 로 인식하는가? 물론 먹히네. 특별한 제약이
+        있지는 않음.
+      </h1>
+    </main>
+  );
+}
